468273b465f03031b9e1f009b31b39c9bf94b8f8
dnvjeyj-dot <dnvjeyj@gmail.com>
2026-09-06 07:52:07 +0800

    Cache always-fresh shortcut launcher

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='debt-manager-shell-v6-payment';
-const ASSETS=['./','./index.html','./app-1.b64','./app-2.b64','./app-3.b64','./app-4.b64','./manifest.webmanifest','./icon.svg'];
+const CACHE='debt-manager-shell-v7-launcher';
+const ASSETS=['./','./index.html','./start.html','./app-1.b64','./app-2.b64','./app-3.b64','./app-4.b64','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
